8cfb8421d3683a584b10aa67b656cfb073f1dda6
Crystal Rivas <cvasq006@odu.edu>
2025-06-25 16:45:44 -0400

    Try it yourself print messages 2-2,2-3

---
+++ Part1_Basics/ch_02_DataTypes/personal_message.py
@@ -0,0 +1,6 @@
+# 2-3 Page 25
+# Use a varaible to represent a person's name, and print that message to that person. 
+first_name = "Rey"
+last_name = "Rivas"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name}")
---
+++ Part1_Basics/ch_02_DataTypes/simple_message.py
@@ -0,0 +1,8 @@
+# 2-2 Page 19 
+# Assign a simple message to a variable, and print that message.
+message = "Hello Crystal"
+print(message)
+
+#Note: 
+# Naming variables: only letters, numbers, and underscores
+# Start with letter or underscore not with a number, no spaces
