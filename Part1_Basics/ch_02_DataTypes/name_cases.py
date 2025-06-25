5f11e39016ebe0d78cd12b1f32442e3bf743353f
Crystal Rivas <cvasq006@odu.edu>
2025-06-25 16:51:50 -0400

    Try it yourself 2-4 name cases

---
+++ Part1_Basics/ch_02_DataTypes/name_cases.py
@@ -0,0 +1,8 @@
+# 2-4 Page 25
+# Use a variable to reprsent a person's name, and then print that person's name in lowercase, uppercase, and title case.
+first_name = "crystal"
+last_name = "Rivas"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}")
+print(f"{full_name.upper()}")
+print(f"{full_name.lower()}")
