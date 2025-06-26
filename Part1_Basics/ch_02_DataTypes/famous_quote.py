9fc509f9eec2d6159482c83276d93227c0243e19
Crystal Rivas <cvasq006@odu.edu>
2025-06-26 16:56:46 -0400

    Try it yourself 2-5 to be continued

---
+++ Part1_Basics/ch_02_DataTypes/famous_quote.py
@@ -0,0 +1,8 @@
+# 2-5, 2-6 Page 25
+# 2-5 Find a quote from a famous person's  name, and then print the quote and name of the author.
+quote = "Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers. - Shakuntala Devi"
+print(f"{quote}")
+print("\"Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers.— Shakuntala Devi"\")
+
+# 2-6 Represent the famous person's name using a variable called famous_person. Then compose
+# the message and represent it with a new variable called message. print message. 
