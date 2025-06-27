bc50979da51564f29b98ddf28a9e3a58100195bb
Crystal Rivas <cvasq006@odu.edu>
2025-06-27 16:37:39 -0400

    Try it yourself 2-8, removesuffix()

---
+++ Part1_Basics/ch_02_DataTypes/file_extensions.py
@@ -0,0 +1,6 @@
+# 2-8 File Extensions page 25
+# Assign the value 'python_notes.txt' to a varaible called filename.
+# Then use the removessuffix() method to display the filename without the file extension. 
+
+filename = 'python_notes.txt'
+print(filename.removesuffix('.txt'))
